84ffe714b2692e3fc093f9a47a99f797da1c9fdb
Vytautas Klimavicius <vyklimavicius@gmail.com>
2019-01-31 23:02:27 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    array.join
+  elsif array.length == 2
+    array.join(' and ')
+  elsif array.length > 2
+    array = array.join(",")
+    array = array.split(",")
+    array = array.join(", ")
+    array = array.split(",")
+    last_word = array.slice!(-1)
+    array = array.insert(-1," and")
+    array = array.join(',')
+    array << last_word
+  end
 end
